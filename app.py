0bcb1e31d55b58f850264f506b44f31a9f1d5714
Oscar M Caicedo <Oscarmc17@Oscars-MacBook-Pro-2.local>
2022-08-01 23:14:04 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+# Parent Class : User
+# Holds details about user
+# Has a function to show user details
+# Child Class : Bank
+# Stores details about the account balance
+# Stores details about the amount 
+# Allows for deposits, withdraws, view_balance
+
+class User:
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+    
