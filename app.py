dfc78ffb54662b0b575a5f173b03fbe0ffa5e8f8
Oscar Caicedo <oscarccd17@gmail.com>
2022-08-04 21:14:49 -0500

    added a withdraw method

--- app.py
+++ app.py
@@ -27,7 +27,22 @@ class Bank(User):
     def __init__(self, name, age, gender):
         super().__init__(name, age, gender)
         self.balance = 0
-        
+
     def deposit(self, amount):
-        self.balance = self.balance + amount
-        print ("Account balance has been udpated $: ", self.balance)
+        self.amount = amount
+        self.balance = self.balance + self.amount
+        print("Account balance has been udpated $: ", self.balance)
+
+    def withdraw(self, amount):
+        self.amount = amount
+        if self.amount > self.balance:
+            print("Insufficient Funds | Balance Available $", self.balance)
+        else:
+            self.balance = self.balance - self.amount
+            print("Account balance has been updated: $", self.balance)
+
+
+person = Bank('Oscar', 28, 'Male')
+person.deposit(10)
+person.withdraw(5)
+person.withdraw(300)
