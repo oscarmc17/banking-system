11bbad48d1ab62aaaae1397976f369de2d3fda88
Oscar Caicedo <oscarccd17@gmail.com>
2022-08-09 22:43:37 -0500

    commented our Bank class

--- app.py
+++ app.py
@@ -23,30 +23,30 @@ class User:
 
 
 # Child Class
-class Bank(User):
-    def __init__(self, name, age, gender):
-        super().__init__(name, age, gender)
-        self.balance = 0
+# class Bank(User):
+#     def __init__(self, name, age, gender):
+#         super().__init__(name, age, gender)
+#         self.balance = 0
 
-    def deposit(self, amount):
-        self.amount = amount
-        self.balance = self.balance + self.amount
-        print("Account balance has been udpated $: ", self.balance)
+#     def deposit(self, amount):
+#         self.amount = amount
+#         self.balance = self.balance + self.amount
+#         print("Account balance has been udpated $: ", self.balance)
 
-    def withdraw(self, amount):
-        self.amount = amount
-        if self.amount > self.balance:
-            print("Insufficient Funds | Balance Available $", self.balance)
-        else:
-            self.balance = self.balance - self.amount
-            print("Account balance has been updated: $", self.balance)
+#     def withdraw(self, amount):
+#         self.amount = amount
+#         if self.amount > self.balance:
+#             print("Insufficient Funds | Balance Available $", self.balance)
+#         else:
+#             self.balance = self.balance - self.amount
+#             print("Account balance has been updated: $", self.balance)
 
-    def view_balance(self):
-        self.show_details()
-        print("Account Balance: $", self.balance)
+#     def view_balance(self):
+#         self.show_details()
+#         print("Account Balance: $", self.balance)
 
 
-person = Bank('Oscar', 28, 'Male')
-person.deposit(1000)
-person.withdraw(900)
-person.view_balance()
+# person = Bank('Oscar', 28, 'Male')
+# person.deposit(1000)
+# person.withdraw(900)
+# person.view_balance()
